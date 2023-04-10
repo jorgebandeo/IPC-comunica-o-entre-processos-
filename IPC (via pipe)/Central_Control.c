c21631ababaae22637e5e752f7c2bc62574a4b9a
Jorge Bandeo <jorgebandeo+Github@hotmail.com>
2023-04-10 19:55:27 -0300

    costrução de cada conceção do servidor com os usuarios

--- IPC (via pipe)/Central_Control.c
+++ IPC (via pipe)/Central_Control.c
@@ -2,8 +2,8 @@
 #include <windows.h>
 
 #define BUFFER_SIZE 1024
-int crear_name_pipe(HANDLE* hPipe){// Create named pipe
-    *hPipe = CreateNamedPipe("\\\\.\\pipe\\pipeso",PIPE_ACCESS_DUPLEX,PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,1,BUFFER_SIZE,BUFFER_SIZE,0,NULL);
+int crear_name_pipe(HANDLE* hPipe, char nome[]){// Create named pipe
+    *hPipe = CreateNamedPipe(nome,PIPE_ACCESS_DUPLEX,PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,1,BUFFER_SIZE,BUFFER_SIZE,0,NULL);
     if (*hPipe == INVALID_HANDLE_VALUE){
         printf("Falha em criar o pipe. Codigo do erro: %d\n", GetLastError());
         return 1;
@@ -21,8 +21,8 @@ int conectividade(HANDLE* hPipe){// Wait for client to connect
     return 0;
 }
 
-int inizializador(HANDLE* hPipe){
-    crear_name_pipe(hPipe);
+int inizializador(HANDLE* hPipe,char nome[]){
+    crear_name_pipe(hPipe, nome );
     printf("Esperando o cliente conectar...\n");
     conectividade(hPipe);
 }
@@ -47,11 +47,27 @@ int escrita_cliente(HANDLE* hPipe, char buffer[BUFFER_SIZE],DWORD* dwRead){
 
 int main()
 {
-    HANDLE hPipe;
-    char buffer[BUFFER_SIZE];
-    DWORD dwRead;
-    inizializador(&hPipe);
+    HANDLE PipeSensorA;
+    HANDLE PipeSensorB;
+    HANDLE PipeDsplay;
+
+    char bufferSensorA[BUFFER_SIZE];
+    char bufferSensorB[BUFFER_SIZE];
+    char bufferDsplay[BUFFER_SIZE];
+
+    DWORD dwReadSensorA;
+    DWORD dwReadSensorB;
+    DWORD dwReadDsplay;
     
+    //Inizialização dos resvidores que vao atender cada ususario
+    inizializador(&PipeSensorA,"\\\\.\\pipe\\SensorA");
+    inizializador(&PipeSensorB,"\\\\.\\pipe\\SensorB");
+    inizializador(&PipeDsplay,"\\\\.\\pipe\\Dsplay");
+    
+
+
+
+
     return 0;
 }
 
